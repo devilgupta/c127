b24e90a5205dbc78f9bfde8b315807185c2000be
devilgupta <57756025+devilgupta@users.noreply.github.com>
2021-05-21 18:00:06 +0530

    Add files via upload

---
+++ scrapper.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+import csv
+
+START_URL="https://exoplanets.nasa.gov/discovery/exoplanet-catalog/"
+browser=webdriver.Chrome("/Users/admin/Downloads/Code_Projects/c127/chromedriver_win32/chromedriver.exe")
+def scrap():
+    headers=["name","light_years_from_earth","planet_mass","stellar_magnitude","discovery_date"]
+    planet_data=[]
+    for i in range(0,439):
+        soup=BeautifulSoup(browser.page_source,"html.parser")
+        for ul_tag in soup.find_all("ul",attrs={"class","exoplanet"}):
+            li_tags=ul_tag.find_all("li")
+            temp_list=[]
+    
+            for index,li_tag in enumerate(li_tags):
+                if index==0:
+                    temp_list.append(li_tag.find_all("a")[0].contents[0])
+                else:
+                    try:
+                        temp_list.append(li_tag.contents[0])
+                    except:
+                        temp_list.append("")
+            planet_data.append(temp_list)
+        browser.find_element_by_xpath('//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
+    with open("scrapper2.csv","w") as f:
+        csvwriter=csv.writer(f)
+        csvwriter.writerow(headers)
+        csvwriter.writerows(planet_data)
+scrap()            
